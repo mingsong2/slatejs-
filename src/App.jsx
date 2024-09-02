336a903924d894cf51934f02400c5477603abdf9
mingsong2 <13732629211@163.com>
2024-09-02 17:55:39 +0800

    自定义选渲染

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React, { useState, useCallback } from 'react'
+import React, { useState, useCallback, useMemo } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { createEditor, Editor, Transforms, Element } from 'slate'
 import { Slate, Editable, withReact } from 'slate-react'
+import CustomEditor from './CustomEditor'
 const CodeElement = props => {
   return (
     <pre {...props.attributes}>
@@ -15,15 +16,31 @@ const CodeElement = props => {
 const DefaultElement = props => {
   return <p {...props.attributes}>{props.children}</p>
 }
+
+const Leaf = props => {
+  return (
+    <span
+      {...props.attributes}
+      style={{ fontWeight: props.leaf.bold ? 'bold' : 'normal' }}
+    >
+      {props.children}
+    </span>
+  )
+}
+
 function App() {
   const [editor] = useState(() => withReact(createEditor()))
   const [count, setCount] = useState(0)
-  const initialValue = [
-    {
-      type: 'paragraph',
-      children: [{ text: 'A line of text in a paragraph.' }],
-    },
-  ]
+  const initialValue = useMemo(
+    () =>
+      JSON.parse(localStorage.getItem('content')) || [
+        {
+          type: 'paragraph',
+          children: [{ text: 'A line of text in a paragraph.' }],
+        },
+      ],
+    []
+  )
   const renderElement = useCallback(props => {
     switch (props.element.type) {
       case 'code':
@@ -32,31 +49,65 @@ function App() {
         return <DefaultElement {...props} />
     }
   }, [])
+  const renderLeaf = useCallback(props => {
+    return <Leaf {...props} />
+  }, [])
   return (
     <div className='editor-wrap'>
-      <Slate editor={editor} initialValue={initialValue}>
+      <Slate 
+        editor={editor} 
+        initialValue={initialValue}
+        onChange={value => {
+          const isAstChange = editor.operations.some(
+            op => 'set_selection' !== op.type
+          )
+          console.log("isAstChange", isAstChange)
+          if (isAstChange) {
+            // Save the value to Local Storage.
+            const content = JSON.stringify(value)
+            localStorage.setItem('content', content)
+          }
+        }}
+      >
+        <div>
+          <button
+            onMouseDown={event => {
+              event.preventDefault()
+              CustomEditor.toggleBoldMark(editor)
+            }}
+          >
+            Bold
+          </button>
+          <button
+            onMouseDown={event => {
+              event.preventDefault()
+              CustomEditor.toggleCodeBlock(editor)
+            }}
+          >
+            Code Block
+          </button>
+        </div>
         <Editable 
           renderElement={renderElement}
+          renderLeaf={renderLeaf}
           onKeyDown={event => {
-            if (event.key === '&') {
-              // Prevent the ampersand character from being inserted.
-              event.preventDefault()
-              // Execute the `insertText` method when the event occurs.
-              editor.insertText('and')
+            if (!event.ctrlKey) {
+              return
             }
-
-            if (event.key === '`' && event.ctrlKey) {
-              event.preventDefault()
-              // Determine whether any of the currently selected blocks are code blocks.
-              const [match] = Editor.nodes(editor, {
-                match: n => n.type === 'code',
-              })
-              // Toggle the block type depending on whether there's already a match.
-              Transforms.setNodes(
-                editor,
-                { type: match ? 'paragraph' : 'code' },
-                { match: n => Element.isElement(n) && Editor.isBlock(editor, n) }
-              )
+  
+            // Replace the `onKeyDown` logic with our new commands.
+            switch (event.key) {
+              case '`': {
+                event.preventDefault()
+                CustomEditor.toggleCodeBlock(editor)
+                break
+              }
+  
+              case 'b': {
+                event.preventDefault()
+                CustomEditor.toggleBoldMark(editor)
+                break
+              }
             }
           }}
         />
