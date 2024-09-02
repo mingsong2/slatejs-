7dcac33325650a9d407b8ba57a936e7189b2c7d6
mingsong2 <13732629211@163.com>
2024-09-02 15:38:46 +0800

    增加slate

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,67 @@
-import { useState } from 'react'
+import React, { useState, useCallback } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import { createEditor, Editor, Transforms, Element } from 'slate'
+import { Slate, Editable, withReact } from 'slate-react'
+const CodeElement = props => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <pre {...props.attributes}>
+      <code>{props.children}</code>
+    </pre>
+  )
+}
+
+const DefaultElement = props => {
+  return <p {...props.attributes}>{props.children}</p>
+}
+function App() {
+  const [editor] = useState(() => withReact(createEditor()))
+  const [count, setCount] = useState(0)
+  const initialValue = [
+    {
+      type: 'paragraph',
+      children: [{ text: 'A line of text in a paragraph.' }],
+    },
+  ]
+  const renderElement = useCallback(props => {
+    switch (props.element.type) {
+      case 'code':
+        return <CodeElement {...props} />
+      default:
+        return <DefaultElement {...props} />
+    }
+  }, [])
+  return (
+    <div className='editor-wrap'>
+      <Slate editor={editor} initialValue={initialValue}>
+        <Editable 
+          renderElement={renderElement}
+          onKeyDown={event => {
+            if (event.key === '&') {
+              // Prevent the ampersand character from being inserted.
+              event.preventDefault()
+              // Execute the `insertText` method when the event occurs.
+              editor.insertText('and')
+            }
+
+            if (event.key === '`' && event.ctrlKey) {
+              event.preventDefault()
+              // Determine whether any of the currently selected blocks are code blocks.
+              const [match] = Editor.nodes(editor, {
+                match: n => n.type === 'code',
+              })
+              // Toggle the block type depending on whether there's already a match.
+              Transforms.setNodes(
+                editor,
+                { type: match ? 'paragraph' : 'code' },
+                { match: n => Element.isElement(n) && Editor.isBlock(editor, n) }
+              )
+            }
+          }}
+        />
+      </Slate>
+    </div>
   )
 }
 
